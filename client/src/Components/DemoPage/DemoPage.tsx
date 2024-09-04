be9171a02d34e3e2d5dbc5d3bf176fd1324ba3d7
Ronit <malvironit1103@gmail.com>
2024-09-04 17:25:45 +0530

    Login Home Menu page

---
+++ client/src/Components/DemoPage/DemoPage.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import projectPlanner from '../../images/project-planner.png';
+import './DemoPage.css';
+import user1 from '../../images/person1.png'
+import user2 from '../../images/person2.png'
+import user3 from '../../images/person3.png'
+import DemoUser from './DemoUser/DemoUser';
+import Login from '../Login/Login';
+
+type DemoPageProps = {
+  logIn: (userID: string) => void
+  setAppError: React.Dispatch<React.SetStateAction<Error | null>>
+}
+
+const DemoPage = ({ logIn, setAppError }: DemoPageProps) => {
+  const userBtns = [{ img: user1, id: '1' }, { img: user2, id: '2' }, { img: user3, id: '3' }].map(user => <DemoUser key={user.id} user={user.img} userID={user.id} logIn={logIn} />)
+  return (
+    <section className='demo-page'>
+      <section className='demo-title demo-text'>
+          <h1 className='welcome-title'>Welcome to the</h1>
+          <img src={projectPlanner} className='demo-img' alt='project planner text' />
+      </section> 
+      <h2 className='demo-text'>Choose an account to demo</h2>
+      <section className='demo-users'>
+        {userBtns}
+      </section>
+      <section className='divider'>
+        <div className='divider-line'></div>
+        <p style={{margin: '2%'}}>OR</p>
+        <div className='divider-line'></div>
+      </section>
+      <div className='login-container'>
+        <Login setAppError={setAppError} logIn={logIn} />
+      </div>
+    </section>
+  )
+}
+
+export default DemoPage
---
+++ client/src/Components/Menu/Menu.tsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+import close from '../../images/close.png';
+import './Menu.css';
+
+interface MenuProps {
+  openOrCloseMenu: () => void;
+  logOut: () => void;
+}
+
+const Menu = ({ openOrCloseMenu, logOut }: MenuProps) => {
+  const logOutFromMenu = () => {
+    logOut();
+    openOrCloseMenu();
+  }
+
+  return (
+    <nav className='menu-nav'>
+      <button className='clear-bg-btn menu-close-btn' onClick={openOrCloseMenu}>
+        <img src={close} alt='menu button' />
+      </button>
+      <Link className='menu-link' to='/'>
+        <button className='clear-bg-btn' onClick={openOrCloseMenu}>
+          Home
+        </button>
+      </Link>
+      <Link className='menu-link' to='/form'>
+        <button className='clear-bg-btn' onClick={openOrCloseMenu}>
+          New Project
+        </button>
+      </Link>
+      <Link className='menu-link' to='/saved'>
+        <button className='clear-bg-btn' onClick={openOrCloseMenu}>
+          Favorite Projects
+        </button>
+      </Link>
+      <Link className='menu-link' to='/history'>
+        <button className='clear-bg-btn' onClick={openOrCloseMenu}>
+          All Projects
+        </button>
+      </Link>
+      <button onClick={logOutFromMenu} className='logout-btn menu-logout'>LOG OUT</button>
+    </nav>
+  );
+};
+
+export default Menu;
